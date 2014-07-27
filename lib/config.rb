9d9188046c50188e87b6d500e44d34f6a77b1db6
Artemiy Solopov <art-solopov@yandex.ru>
2014-07-28 02:28:45 +0400

    Pulling out the configuration parameters; working on the interactive dev console

---
+++ lib/config.rb
@@ -0,0 +1,31 @@
+# Configuration files and app-wide requires go here
+
+require 'sinatra'
+require 'data_mapper'
+require 'model/bill'
+require 'model/category'
+configure :production do
+  DataMapper::Logger.new('db-log', :debug)
+  DataMapper.setup(:default,
+    'postgres://billy-bones:billy@localhost/billy-bones')
+  DataMapper.finalize
+end
+
+configure :development do
+  DataMapper::Logger.new($stderr, :debug)
+  DataMapper.setup(:default,
+    'postgres://billy-bones:billy@localhost/billy-bones-dev')
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
+end
+
+configure :test do
+  require 'dm_migrations'
+  DataMapper::Logger.new($stderr, :debug)
+  DataMapper.setup(:default,
+    'postgres://billy-bones:billy@localhost/billy-bones-test')
+  DataMapper.finalize
+  DataMapper.auto_migrate!
+end
+
+
--- test/model_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'data_mapper'
-DataMapper::Logger.new($stderr, :debug)
-DataMapper.setup(:default,
-  'postgres://billy-bones:billy@localhost/billy-bones-test')
-
-require 'minitest/autorun'
-require 'dm-migrations'
-DataMapper.auto_migrate!
