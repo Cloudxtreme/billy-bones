25948bfddaf1a6ab86eebc6eb22006885002778a
Artemiy Solopov <art-solopov@yandex.ru>
2014-08-02 02:43:24 +0400

    Added Category subclasses, fixed everything that goes with it.

--- lib/model/bill.rb
+++ lib/model/bill.rb
@@ -5,8 +5,8 @@ class Bill
   include DataMapper::Resource
 
   def template
-      # Name of template that should be used to render the object
-      nil
+    # Name of template that should be used to render the object
+    nil
   end
 
   property :id, Serial
@@ -14,35 +14,53 @@ class Bill
 
   belongs_to :category
 
+  def to_hash
+    # Returns a hash containing all attributes
+    # for view representation
+    attrs = attributes
+    attrs[:category] = category.name
+    attrs
+  end
+
 end
 
 class ReferentialBill < Bill
-    # Referential bills only list the amount of resource (e.g. water,
-    # electricity and such) consumed.
-    property :meter, Float, required: true
+  # Referential bills only list the amount of resource (e.g. water,
+  # electricity and such) consumed.
+  property :meter, Float
+  property :measure_date, Date
+  validates_presence_of :meter, :measure_date
 
-    def template
-        'referential_bill'
-    end
+  def template
+    :referential_bill
+  end
+
 end
 
 class PredefinedBill < Bill
-    # Predefined bills have a cost that doesn't depend on the amount of
-    # resource consumed
-    property :cost, Float, required: true
+  # Predefined bills have a cost that doesn't depend on the amount of
+  # resource consumed
+  property :cost, Float
+  property :pay_date, Date
+  validates_presence_of :cost, :pay_date
 
-    def template
-        'predefined_bill'
-    end
+  def template
+    :predefined_bill
+  end
 end
 
-class ComputableBill < ReferentialBill
-    # Computable bills have a cost that depends on the amount of resource
-    # consumed and a tariff. The cost should not be set manually,
-    # but computed using the tariff, the meter value and the modificators
-    # (if any).
-    #belongs_to :tariff TODO uncomment when tariff ready
-    property :cost, Float, writer: :private
-    property :modificators, Json, default: '[]'
-end
+class ComputableBill < Bill
+  # Computable bills have a cost that depends on the amount of resource
+  # consumed and a tariff. The cost should not be set manually,
+  # but computed using the tariff, the meter value and the category's
+  # modificators (if any).
+  #belongs_to :tariff TODO uncomment when tariff ready
+  property :cost, Float, writer: :private
+  property :pay_date, Date
+  property :measure_date, Date
+  validates_presence_of :pay_date
 
+  def template
+    :computable_bill
+  end
+end
--- lib/model/category.rb
+++ lib/model/category.rb
@@ -1,25 +1,39 @@
 require 'data_mapper'
+require 'active_support/inflector'
 
 class Category
     include DataMapper::Resource
 
     property :id, Serial
     property :name, String
-    property :type, Enum[:referential, :predefined, :computable]
+    property :cat_type, Discriminator
 
-    has n, :bills
     # has n, :tariffs TODO uncomment when tariff ready
 
     def create_bill(params)
       # A bill factory for current category type
-      case type
-      when :referential
-        ReferentialBill.new params
-      when :predefined
-        PredefinedBill.new params
-      when :computable
-        ComputableBill.new params
-      end
+      new_bill = eval("#{self.class.bill_type}").new(params)
+      new_bill.category = self
+      new_bill
     end
 
+    def self.bill_template(name)
+      "_#{bill_type.underscore}_#{name}"
+    end
+
+end
+
+%w{Referential Predefined Computable}.each do |bill_type|
+  eval %{
+    class #{bill_type}Category < Category
+      def self.bill_type
+        '#{bill_type}Bill'
+      end
+
+      has n, :#{bill_type.downcase}_bills, child_key: [:category_id]
+
+      alias :bills :#{bill_type.downcase}_bills
+
+    end
+  }
 end
