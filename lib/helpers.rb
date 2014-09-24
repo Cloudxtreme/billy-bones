ef960d8811c37db314ba183435b894832a24d862
Artemiy Solopov <art-solopov@yandex.ru>
2014-09-24 22:26:22 +0400

    Added bill adding and editing

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -4,6 +4,7 @@ module Helpers
 
     def update_params(model, params)
       params.each do |key, value|
+        if value == '' then value = nil end
         model.attribute_set key, value
       end
       model.save
